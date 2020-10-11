b52c85d891fe947289413defe5c3c80422ee335e
Ed Hassinger <ehassinger@r4.ai>
2020-10-10 23:06:34 -0500

    Add project files.

---
+++ Employee/Migrations/20201010200248_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Employee.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Salary = table.Column<short>(nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ Employee/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public Int16 Salary { get; set; }
+
+        // public List<string> dependants { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+    }
+}
